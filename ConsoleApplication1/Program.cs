3224eab396b7426aa6d9cda6cd8bc96edc06d3b3
Vladislav <vmel315@gmail.com>
2016-09-29 15:57:44 +0300

    QSort

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,13 +4,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace QSort
 {
     class Program
     {
         static void Main(string[] args)
         {
-            qwerty
+            Random rand = new Random();
+
+            int[] arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = rand.Next(-100, 100);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " | ");
+            }
+
+            QuickSort<int> q = new QuickSort<int>();
+            Console.WriteLine("\n");
+            q.Sort(arr, 0, arr.Length - 1);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " | ");
+            }
+            Console.Read();
+        }
+    }
+    public class QuickSort<T> where T : IComparable<T>
+    {
+        public T Piviot { get; set; }
+        public void Sort(T[] elements, int left, int right)
+        {
+            int i = left, j = right;
+            Piviot = elements[(left + right) / 2];
+            while (i <= j)
+            {
+                while (elements[i].CompareTo(Piviot) < 0)
+                    i++;
+                while (elements[j].CompareTo(Piviot) > 0)
+                    j--;
+                if (i <= j)
+                {
+                    Swap(ref elements[i], ref elements[j]);
+                    i++;
+                    j--;
+                }
+            }
+            if (left < j)
+            {
+                Sort(elements, left, j);
+            }
+            if (i < right)
+            {
+                Sort(elements, i, right);
+            }
+        }
+
+        public void Swap(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
         }
     }
 }
