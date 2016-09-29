8f6795e6ba296e17875fba5bcb37504253f8dc19
Vladislav <vmel315@gmail.com>
2016-09-29 16:32:26 +0300

    OtherSorting

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -8,12 +8,30 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestSorting()
+        public void TestBaseSort()
         {
             int[] arr = { 5, 3, 2, 4, 1 };
             QuickSort<int> qsort = new QuickSort<int>();
             qsort.Sort(arr, 0, 4);
             int[] expArr = { 1, 2, 3, 4, 5};
+            CollectionAssert.AreEqual(arr, expArr);
+        }
+        [TestMethod]
+        public void TestDoubNum()
+        {
+            double[] arr = { 5.2, 3.1, 2, 4.9, 1 };
+            QuickSort<double> qsort = new QuickSort<double>();
+            qsort.Sort(arr, 0, 4);
+            double[] expArr = { 1, 2, 3.1, 4.9, 5.2 };
+            CollectionAssert.AreEqual(arr, expArr);
+        }
+        [TestMethod]
+        public void TestLetNum()
+        {
+            string[] arr = { "a", "c", "d", "e", "b" };
+            QuickSort<string> qsort = new QuickSort<string>();
+            qsort.Sort(arr, 0, 4);
+            string[] expArr = { "a", "b", "c", "d", "e" }; ;
             CollectionAssert.AreEqual(arr, expArr);
         }
     }
