d706262789f31b214f9c70645c7fd446ad8fe516
Vladislav <vmel315@gmail.com>
2016-09-29 16:41:04 +0300

    OtherSort_2

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -26,12 +26,39 @@ namespace UnitTestProject1
             CollectionAssert.AreEqual(arr, expArr);
         }
         [TestMethod]
-        public void TestLetNum()
+        public void TestLet()
         {
             string[] arr = { "a", "c", "d", "e", "b" };
             QuickSort<string> qsort = new QuickSort<string>();
             qsort.Sort(arr, 0, 4);
             string[] expArr = { "a", "b", "c", "d", "e" }; ;
+            CollectionAssert.AreEqual(arr, expArr);
+        }
+        [TestMethod]
+        public void TestLetAndNum()
+        {
+            string[] arr = { "a", "e", "4", "b", "1" };
+            QuickSort<string> qsort = new QuickSort<string>();
+            qsort.Sort(arr, 0, 4);
+            string[] expArr = { "1", "4", "a", "b", "e" }; ;
+            CollectionAssert.AreEqual(arr, expArr);
+        }
+        [TestMethod]
+        public void TestBelZero()
+        {
+            int[] arr = { -5, -9, -20, -1, -14 };
+            QuickSort<int> qsort = new QuickSort<int>();
+            qsort.Sort(arr, 0, 4);
+            int[] expArr = { -20, -14, -9, -5, -1 }; ;
+            CollectionAssert.AreEqual(arr, expArr);
+        }
+        [TestMethod]
+        public void TessSig()
+        {
+            string[] arr = { "(", "-", ")" };
+            QuickSort<string> qsort = new QuickSort<string>();
+            qsort.Sort(arr, 0, 1);
+            string[] expArr = { "-", "(", ")" };
             CollectionAssert.AreEqual(arr, expArr);
         }
     }
